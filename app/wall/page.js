4543b51d6b13872d561d86e299a062328bacafa9
JohnCarloReyes22 <22-03171@g.batstate-u.edu.ph>
2026-08-27 21:34:37 +0800

    change of placeholder background

--- app/wall/page.js
+++ app/wall/page.js
@@ -78,7 +78,7 @@ export default function WallPage() {
               {grouped[tier].map((p) =>
                 tier === '4' ? (
                   <div key={p.id} className="card-border-bronze">
-                    <div className="bg-white rounded-lg p-4">
+                    <div className="bg-[#FDF6EC] rounded-lg p-4">
                       <PersonCard person={p} />
                     </div>
                   </div>
